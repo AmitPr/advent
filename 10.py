b0df41e3009e83d040c52ce2fd1a9739f9b1536b
Amit Prasad <17amitprasad@gmail.com>
2019-12-10 16:14:49 -0500

    Cleaned up Day 10, added an IntCode class used to clean up the IntCode Computer

--- 10.py
+++ 10.py
@@ -2,19 +2,6 @@ import numpy as np
 import math
 import collections
 
-def quad(p):
-    x,y=p
-    if y>=0:
-        if x >=0:
-            return 1
-        else:
-            return 2
-    else:
-        if x >=0:
-            return 4
-        else:
-            return 3
-
 with open("input10") as f:
     lines = f.readlines()
 map = [[1 if x == '#' else 0 for x in line] for line in lines]
@@ -44,7 +31,7 @@ for y in range(len(map)):
             if len(seen)>len(maxSeen):
                 maxSeen=seen
                 p=(x,y)
-print(len(maxSeen))
+print("Part 1:",len(maxSeen))
 m = [math.atan2(x[1],x[0]) for x in maxSeen]
 x = zip(m,maxSeen)
 d =  list(collections.OrderedDict(sorted(x,key = lambda x :(math.degrees(-1*(2*math.pi+x[0]))+90 if x[0] < 0 else math.degrees(-1*x[0])+90) if (math.degrees(-1*(2*math.pi+x[0]))+90 if x[0] < 0 else math.degrees(-1*x[0])+90)<0 else (math.degrees(-1*(2*math.pi+x[0]))+90 if x[0] < 0 else math.degrees(-1*x[0])+90)-360)).values())
@@ -52,4 +39,4 @@ k = d[199]
 x,y = p[0],p[1]
 x+=k[0]
 y-=k[1]
-print(x*100+y)
+print("Part 2:",x*100+y)
